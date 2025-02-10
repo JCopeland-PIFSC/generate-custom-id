98e57e8e8e239eddf19fbf391c6387675e66166e
Jesse Copeland <jesse.copeland@noaa.gov>
2025-02-10 10:18:32 -1000

    Add numSegments to collision test

--- test/generateCustomIdCollisionTest.js
+++ test/generateCustomIdCollisionTest.js
@@ -1,8 +1,9 @@
 import { performance } from "perf_hooks";
 import readline from "readline";
-import generateCustomId from "../generateCustomId.js";
+import { generateCustomId } from "../generateCustomId.js";
 
 const segmentLength = 12; // You can change this to test different segment lengths
+const numSegments = 1; // You can change this to test different numbers of segments
 const iterations = 1_000_000; // You can change this to test different numbers of iterations
 const ids = new Set(); // !!! The maximum size of a Set in Chrome and node.js is 16,777,216 (2^24) !!!
 
@@ -14,27 +15,29 @@ console.log(
 
 const start = performance.now();
 
-(async () => {
-  const generateId = generateCustomId({ prefix: "ID", segmentLength });
-  for (let i = 0; i < iterations; i++) {
-    const id = await generateId();
+const generateId = generateCustomId({
+  prefix: "ID",
+  segmentLength,
+  numSegments,
+});
+for (let i = 0; i < iterations; i++) {
+  const id = generateId();
 
-    // Update the console with the current ID
-    readline.cursorTo(process.stdout, 0);
-    process.stdout.write(`Current ID: ${id}`);
+  // Update the console with the current ID
+  readline.cursorTo(process.stdout, 0);
+  process.stdout.write(`Current ID: ${id}`);
 
-    if (ids.has(id)) {
-      collisions++;
-    } else {
-      ids.add(id);
-    }
+  if (ids.has(id)) {
+    collisions++;
+  } else {
+    ids.add(id);
   }
+}
 
-  const end = performance.now();
-  const duration = end - start;
+const end = performance.now();
+const duration = end - start;
 
-  console.log("\n"); // Move to the next line after the loop
-  console.log(`Total collisions: ${collisions}`);
-  console.log(`Collision rate: ${(collisions / iterations) * 100}%`);
-  console.log(`Time taken: ${duration.toFixed(2)}ms`);
-})();
+console.log("\n"); // Move to the next line after the loop
+console.log(`Total collisions: ${collisions}`);
+console.log(`Collision rate: ${(collisions / iterations) * 100}%`);
+console.log(`Time taken: ${duration.toFixed(2)}ms`);
