b70d08541ad8037e23bfb77a758c8c51400d4d6e
Jesse Copeland <jesse.copeland@noaa.gov>
2025-02-10 10:17:55 -1000

    Add test for new options

--- test/generateCustomId.test.js
+++ test/generateCustomId.test.js
@@ -1,144 +1,225 @@
-import { expect } from "chai";
-import chaiAsPromised from "chai-as-promised";
 import { describe, it, beforeAll } from "vitest";
 import { performance } from "perf_hooks";
-import generateCustomId from "../generateCustomId.js";
+import { generateCustomId, validateCheckBit } from "../generateCustomId.js";
 
-beforeAll(() => {
-  chai.use(chaiAsPromised);
+let expect;
+
+beforeAll(async () => {
+  const chai = await import("chai");
+  expect = chai.expect;
 });
 
 describe("generateCustomId", function () {
-  const segmentLengths = [8, 9, 10, 11, 12, 13, 14, 15];
-  const delimiters = ["-", "_", "|", ".", "#", "none"];
+  const segmentLengthRanges = {
+    1: { min: 8, max: 15 },
+    2: { min: 5, max: 10 },
+    3: { min: 4, max: 8 },
+    4: { min: 3, max: 6 },
+  };
+  const delimiters = ["-", "_", "|", ".", "#", null];
 
-  segmentLengths.forEach((length) => {
-    it(`should generate a unique ID with segment length ${length} and no date`, async function () {
-      const generateId = generateCustomId({
-        prefix: "ID",
-        segmentLength: length,
-        includeDate: false,
-      });
-      const id = await generateId();
-      expect(id).to.match(new RegExp(`^ID-[A-Z0-9]{${length}}$`));
-    });
-
-    it(`should generate a unique ID with segment length ${length} and date`, async function () {
-      const generateId = generateCustomId({
-        prefix: "ID",
-        segmentLength: length,
-        includeDate: true,
-      });
-      const id = await generateId();
-      expect(id).to.match(new RegExp(`^ID-\\d{8}-[A-Z0-9]{${length}}$`));
-    });
-
-    it(`should generate a unique ID with segment length ${length} and full timestamp`, async function () {
-      const generateId = generateCustomId({
-        prefix: "ID",
-        segmentLength: length,
-        includeDate: true,
-        useTimestamp: true,
-      });
-      const id = await generateId();
-      expect(id).to.match(new RegExp(`^ID-\\d{8}T\\d{6}-[A-Z0-9]{${length}}$`));
-    });
-
-    delimiters.forEach((delimiter) => {
-      it(`should generate a unique ID with segment length ${length}, date, and delimiter "${delimiter}"`, async function () {
+  Object.keys(segmentLengthRanges).forEach((numSegments) => {
+    const { min, max } = segmentLengthRanges[numSegments];
+    for (let length = min; length <= max; length++) {
+      it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, and no date`, function () {
         const generateId = generateCustomId({
           prefix: "ID",
           segmentLength: length,
-          includeDate: true,
-          delimiter,
+          numSegments: parseInt(numSegments),
+          includeDate: false,
         });
-        const id = await generateId();
-        const delimiterChar = delimiter === "none" ? "" : delimiter;
-        expect(id).to.match(
-          new RegExp(
-            `^ID${delimiterChar}\\d{8}${delimiterChar}[A-Z0-9]{${length}}$`
-          )
+        const id = generateId();
+        const regex = new RegExp(
+          `^ID-${Array(parseInt(numSegments))
+            .fill(`[A-Z0-9]{${length}}`)
+            .join("-")}$`
         );
+        expect(id).to.match(regex);
       });
-    });
 
-    it(`should generate a unique ID with segment length ${length} and lowercase random segment`, async function () {
-      const generateId = generateCustomId({
-        prefix: "ID",
-        segmentLength: length,
-        lowercase: true,
+      it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, and date`, function () {
+        const generateId = generateCustomId({
+          prefix: "ID",
+          segmentLength: length,
+          numSegments: parseInt(numSegments),
+          includeDate: true,
+        });
+        const id = generateId();
+        const regex = new RegExp(
+          `^ID-\\d{8}-${Array(parseInt(numSegments))
+            .fill(`[A-Z0-9]{${length}}`)
+            .join("-")}$`
+        );
+        expect(id).to.match(regex);
       });
-      const id = await generateId();
-      expect(id).to.match(new RegExp(`^ID-\\d{8}-[a-z0-9]{${length}}$`));
-    });
+
+      it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, and full timestamp`, function () {
+        const generateId = generateCustomId({
+          prefix: "ID",
+          segmentLength: length,
+          numSegments: parseInt(numSegments),
+          includeDate: true,
+          useTimestamp: true,
+        });
+        const id = generateId();
+        const regex = new RegExp(
+          `^ID-\\d{8}T\\d{6}-${Array(parseInt(numSegments))
+            .fill(`[A-Z0-9]{${length}}`)
+            .join("-")}$`
+        );
+        expect(id).to.match(regex);
+      });
+
+      delimiters.forEach((delimiter) => {
+        it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, date, and delimiter "${delimiter}"`, function () {
+          const generateId = generateCustomId({
+            prefix: "ID",
+            segmentLength: length,
+            numSegments: parseInt(numSegments),
+            includeDate: true,
+            delimiter: delimiter,
+          });
+          const id = generateId();
+          const delimiterChar = delimiter === null ? "" : delimiter;
+          const regex = new RegExp(
+            `^ID${delimiterChar}\\d{8}${delimiterChar}${Array(
+              parseInt(numSegments)
+            )
+              .fill(`[A-Z0-9]{${length}}`)
+              .join(delimiterChar)}$`
+          );
+          expect(id).to.match(regex);
+        });
+      });
+
+      it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, and lowercase random segment`, function () {
+        const generateId = generateCustomId({
+          prefix: "ID",
+          segmentLength: length,
+          numSegments: parseInt(numSegments),
+          lowercase: true,
+        });
+        const id = generateId();
+        const regex = new RegExp(
+          `^ID-\\d{8}-${Array(parseInt(numSegments))
+            .fill(`[a-z0-9]{${length}}`)
+            .join("-")}$`
+        );
+        expect(id).to.match(regex);
+      });
+
+      it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, postfix, and check bit`, function () {
+        const generateId = generateCustomId({
+          prefix: "ID",
+          segmentLength: length,
+          numSegments: parseInt(numSegments),
+          postfix: "POST",
+          includeCheckBit: true,
+        });
+        const id = generateId();
+        const regex = new RegExp(
+          `^ID-\\d{8}-${Array(parseInt(numSegments))
+            .fill(`[A-Z0-9]{${length}}`)
+            .join("-")}-POST-[A-Z0-9]$`
+        );
+        expect(id).to.match(regex);
+        const isValid = validateCheckBit(id);
+        expect(isValid).to.be.true;
+      });
+
+      it(`should generate a unique ID with ${numSegments} segment(s), segment length ${length}, no delimiter, and check bit`, function () {
+        const generateId = generateCustomId({
+          prefix: "ID",
+          segmentLength: length,
+          numSegments: parseInt(numSegments),
+          delimiter: null,
+          includeCheckBit: true,
+        });
+        const id = generateId();
+        const regex = new RegExp(
+          `^ID\\d{8}${Array(parseInt(numSegments))
+            .fill(`[A-Z0-9]{${length}}`)
+            .join("")}[A-Z0-9]$`
+        );
+        expect(id).to.match(regex);
+        const isValid = validateCheckBit(id);
+        expect(isValid).to.be.true;
+      });
+    }
   });
 
-  it("should generate a unique ID with default parameters", async function () {
+  it("should generate a unique ID with default parameters", function () {
     const generateId = generateCustomId();
-    const id = await generateId();
+    const id = generateId();
     expect(id).to.match(/^ID-\d{8}-[A-Z0-9]{12}$/);
   });
 
-  it("should generate a unique ID with a custom prefix", async function () {
+  it("should generate a unique ID with a custom prefix", function () {
     const generateId = generateCustomId({ prefix: "A" });
-    const id = await generateId();
+    const id = generateId();
     expect(id).to.match(/^A-\d{8}-[A-Z0-9]{12}$/);
   });
 
-  it("should generate a unique ID with a custom segment length", async function () {
+  it("should generate a unique ID with a custom segment length", function () {
     const generateId = generateCustomId({ segmentLength: 10 });
-    const id = await generateId();
+    const id = generateId();
     expect(id).to.match(/^ID-\d{8}-[A-Z0-9]{10}$/);
   });
 
-  it("should generate a unique ID with custom options", async function () {
+  it("should generate a unique ID with custom options", function () {
     const generateId = generateCustomId({ delimiter: "|" });
-    const id = await generateId();
+    const id = generateId();
     expect(id).to.match(/^ID\|\d{8}\|[A-Z0-9]{12}$/);
   });
 
-  it("should generate a unique ID with a custom prefix and segment length", async function () {
+  it("should generate a unique ID with a custom prefix and segment length", function () {
     const generateId = generateCustomId({ prefix: "A", segmentLength: 13 });
-    const id = await generateId();
+    const id = generateId();
     expect(id).to.match(/^A-\d{8}-[A-Z0-9]{13}$/);
   });
 
-  it("should generate a unique ID with a custom prefix and options", async function () {
+  it("should generate a unique ID with a custom prefix and options", function () {
     const generateId = generateCustomId({ prefix: "B", useTimestamp: true });
-    const id = await generateId();
+    const id = generateId();
     expect(id).to.match(/^B-\d{8}T\d{6}-[A-Z0-9]{12}$/);
   });
 
-  it("should throw an error for invalid segment length (7)", async function () {
+  it("should generate a unique ID with multiple segments", function () {
+    const generateId = generateCustomId({ numSegments: 2, segmentLength: 5 });
+    const id = generateId();
+    expect(id).to.match(/^ID-\d{8}-[A-Z0-9]{5}-[A-Z0-9]{5}$/);
+  });
+
+  it("should throw an error for invalid segment length (7)", function () {
     expect(() => generateCustomId({ prefix: "ID", segmentLength: 7 })).to.throw(
-      "Segment length must be an integer between 8 and 15"
+      "Segment length for 1 segments must be between 8 and 15"
     );
   });
 
-  it("should throw an error for invalid segment length (16)", async function () {
+  it("should throw an error for invalid segment length (16)", function () {
     expect(() =>
       generateCustomId({ prefix: "ID", segmentLength: 16 })
-    ).to.throw("Segment length must be an integer between 8 and 15");
+    ).to.throw("Segment length for 1 segments must be between 8 and 15");
   });
 
-  it("should throw an error for decimal segment length", async function () {
+  it("should throw an error for decimal segment length", function () {
     expect(() =>
       generateCustomId({ prefix: "ID", segmentLength: 10.5 })
-    ).to.throw("Segment length must be an integer between 8 and 15");
+    ).to.throw("Segment length must be an integer");
   });
 
-  it("should throw an error for string segment length", async function () {
+  it("should throw an error for string segment length", function () {
     expect(() =>
       generateCustomId({ prefix: "ID", segmentLength: "12" })
-    ).to.throw("Segment length must be an integer between 8 and 15");
+    ).to.throw("Segment length must be an integer");
   });
 
-  it("should measure the performance of the function", async function () {
+  it("should measure the performance of the function", function () {
     const generateId = generateCustomId();
     const iterations = 1000;
     const start = performance.now();
     for (let i = 0; i < iterations; i++) {
-      await generateId();
+      generateId();
     }
     const end = performance.now();
     const duration = end - start;
